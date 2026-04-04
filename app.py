859096a071813dc2efe44d6d889c15bd222671b2
rakesh gorre <rakhig123786@gmail.com>
2026-04-04 23:52:55 +0530

    chat-gpt task 6

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import requests
+import json
 print("Pipeline running successfully")
 print('Inside Python Script')
 api_key = os.getenv('api_key')
@@ -8,3 +10,14 @@ if not api_key1:
 if api_key:
     print('api_key is loaded')
     print(len(api_key))
+url = os.getenv('url')
+print(f"url : {url}")
+api_data = requests.get(url)
+print(f"status code for api hit {api_data.status_code}")
+if api_data.status_code == 200:
+    json_data = api_data.json()
+    with open('test_data_saved/api_octocat.json', 'w') as writer:
+        json.dump(json_data, writer, indent=4)
+    print("file saved sucess fully at test_data_saved/api_octocat.json")
+else:
+    print(f'API hit failed for {url}')
